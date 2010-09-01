f08f164ee5c676ec45908b704c57117bb77dd4a6
Ryan Campbell <ryan.campbell@gmail.com>
2010-09-01 08:33:19 -0500

    initial commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+This project is a project which provides a "relaeased" core project.  It provides a Logger class to be consumed by dependent projects.
---
+++ src/main/java/com/cloudbees/mvntest/core/Logger.java
@@ -0,0 +1,13 @@
+package  com.cloudbees.mvntest.core;
+
+/**
+ * Core logger class.
+ *
+ */
+public class Logger 
+{
+    public static void log(String msg)
+    {
+        System.out.println( msg );
+    }
+}
