5c34baefaddd59bcc3fe32c3c5c141b658d376b5
Ryan Campbell <ryan.campbell@gmail.com>
2012-06-01 10:43:17 -0500

    remove dep on util

--- src/main/java/com/cloudbees/mvntest/core/Logger.java
+++ src/main/java/com/cloudbees/mvntest/core/Logger.java
@@ -9,7 +9,6 @@ public class Logger
     private Object unused;
     public static void log(String msg)
     {
-        assert com.cloudbees.mvntest.util.Math.subtract(2,1).equals(1);
         System.out.println( msg + "hello" );
     }
 }
