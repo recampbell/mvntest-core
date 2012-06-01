8f422b3526f49895a22e83f68daf89e8605f5f56
Ryan Campbell <ryan.campbell@gmail.com>
2012-06-01 10:40:27 -0500

    add a call to Math

--- src/main/java/com/cloudbees/mvntest/core/Logger.java
+++ src/main/java/com/cloudbees/mvntest/core/Logger.java
@@ -9,6 +9,7 @@ public class Logger
     private Object unused;
     public static void log(String msg)
     {
+        assert com.cloudbees.mvntest.util.Math.add(1,2).equals(3);
         System.out.println( msg + "hello" );
     }
 }
