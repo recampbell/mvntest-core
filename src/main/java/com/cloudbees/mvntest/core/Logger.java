a7138b3435d57967f01af513f1051aa55e6caf80
Ryan Campbell <ryan.campbell@gmail.com>
2012-06-01 10:42:07 -0500

    call subract instead

--- src/main/java/com/cloudbees/mvntest/core/Logger.java
+++ src/main/java/com/cloudbees/mvntest/core/Logger.java
@@ -9,7 +9,7 @@ public class Logger
     private Object unused;
     public static void log(String msg)
     {
-        assert com.cloudbees.mvntest.util.Math.add(1,2).equals(3);
+        assert com.cloudbees.mvntest.util.Math.subtract(2,1).equals(1);
         System.out.println( msg + "hello" );
     }
 }
